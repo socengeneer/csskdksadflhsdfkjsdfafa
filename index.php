9e5c343321931a11c00a03b33ea145836182af9d
socengeneer <104505178+socengeneer@users.noreply.github.com>
2022-04-27 21:50:35 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html lang="ru">
+
+<head>
+
+<meta charset="UTF-8">
+
+<title>Пример веб-страницы с php кодом</title>
+
+</head>
+
+<body>
+
+<?php
+echo $_SERVER['REMOTE_ADDR'];
+?>
+
+</body>
+
+</html>
